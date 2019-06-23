a8de4c2c568770b8a3204bd99a99c5ba5023e812
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2019-06-23 23:14:13 +0200

    Fix coding style.

--- tests/autoload.php
+++ tests/autoload.php
@@ -5,9 +5,11 @@
  * Customizes auto-loading so that namespaces of modules are registered.
  */
 
+use Composer\Autoload\ClassLoader;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$classLoader = new \Composer\Autoload\ClassLoader();
+$classLoader = new ClassLoader();
 $classLoader->addPsr4("Drupal\\file\\", __DIR__ . '/../vendor/drupal/core/modules/file/src');
 $classLoader->addPsr4("Drupal\\user\\", __DIR__ . '/../vendor/drupal/core/modules/user/src');
 $classLoader->register();
