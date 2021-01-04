11c1a526087cd87631ee39664762ed5ef7724f0d
Petar <petar.basic@drunomics.com>
2021-01-04 16:01:50 +0100

    LDP-606: Add path_alias to classpath.

--- tests/autoload.php
+++ tests/autoload.php
@@ -11,6 +11,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $classLoader = new ClassLoader();
 $classLoader->addPsr4("Drupal\\file\\", __DIR__ . '/../vendor/drupal/core/modules/file/src');
+$classLoader->addPsr4("Drupal\\path_alias\\", __DIR__ . '/../vendor/drupal/core/modules/path_alias/src');
 $classLoader->addPsr4("Drupal\\user\\", __DIR__ . '/../vendor/drupal/core/modules/user/src');
 $classLoader->addPsr4("Drupal\\pathauto\\", __DIR__ . '/../vendor/drupal/pathauto/src');
 $classLoader->register();
