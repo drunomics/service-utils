a27d204ea91e1ed701508f4ae72fb6a80f81fbb0
ciprian <ciprian@drunomics.com>
2022-01-31 19:13:07 +0200

    LDP-1157: Fix code indentation.

--- src/Core/Language/LanguageManagerTrait.php
+++ src/Core/Language/LanguageManagerTrait.php
@@ -8,37 +8,38 @@ use Drupal\Core\Language\LanguageManagerInterface;
  * Allows setter injection and simple usage of the service.
  */
 trait LanguageManagerTrait {
-    /**
-     * The language mamnager.
-     *
-     * @var \Drupal\Core\Language\LanguageManagerInterface
-     */
-    protected $languageManager;
 
-    /**
-     * Sets the language manager.
-     *
-     * @param \Drupal\Core\Language\LanguageManagerInterface $languageManager
-     *   The language manager.
-     *
-     * @return $this
-     */
-    public function setLanguageManager(LanguageManagerInterface $languageManager) {
-        $this->languageManager = $languageManager;
-        return $this;
-    }
+  /**
+   * The language mamnager.
+   *
+   * @var \Drupal\Core\Language\LanguageManagerInterface
+   */
+  protected $languageManager;
 
-    /**
-     * Gets the language manager.
-     *
-     * @return \Drupal\Core\Language\LanguageManagerInterface
-     *   The language manager.
-     */
-    public function getLanguageManager() {
-        if (empty($this->languageManager)) {
-            $this->languageManager = \Drupal::service('language_manager');
-        }
-        return $this->languageManager;
+  /**
+   * Sets the language manager.
+   *
+   * @param \Drupal\Core\Language\LanguageManagerInterface $languageManager
+   *   The language manager.
+   *
+   * @return $this
+   */
+  public function setLanguageManager(LanguageManagerInterface $languageManager) {
+    $this->languageManager = $languageManager;
+    return $this;
+  }
+
+  /**
+   * Gets the language manager.
+   *
+   * @return \Drupal\Core\Language\LanguageManagerInterface
+   *   The language manager.
+   */
+  public function getLanguageManager() {
+    if (empty($this->languageManager)) {
+      $this->languageManager = \Drupal::service('language_manager');
     }
+    return $this->languageManager;
+  }
 
 }
