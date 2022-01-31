b7dc2a2985c89e89159887c95970104621a41e71
ciprian <ciprian@drunomics.com>
2022-01-31 19:09:05 +0200

    LDP-1157: Fixes for trait and add trait tests.

--- src/Core/Language/LanguageManagerTrait.php
+++ src/Core/Language/LanguageManagerTrait.php
@@ -4,34 +4,37 @@ namespace drunomics\ServiceUtils\Core\Language;
 
 use Drupal\Core\Language\LanguageManagerInterface;
 
+/**
+ * Allows setter injection and simple usage of the service.
+ */
 trait LanguageManagerTrait {
     /**
-     * The config factory.
+     * The language mamnager.
      *
      * @var \Drupal\Core\Language\LanguageManagerInterface
      */
     protected $languageManager;
 
     /**
-     * Sets the config factory.
+     * Sets the language manager.
      *
      * @param \Drupal\Core\Language\LanguageManagerInterface $languageManager
-     *   The config factory.
+     *   The language manager.
      *
      * @return $this
      */
-    public function setConfigFactory(LanguageManagerInterface $languageManager) {
+    public function setLanguageManager(LanguageManagerInterface $languageManager) {
         $this->languageManager = $languageManager;
         return $this;
     }
 
     /**
-     * Gets the config factory.
+     * Gets the language manager.
      *
      * @return \Drupal\Core\Language\LanguageManagerInterface
-     *   The config factory.
+     *   The language manager.
      */
-    public function getConfigFactory() {
+    public function getLanguageManager() {
         if (empty($this->languageManager)) {
             $this->languageManager = \Drupal::service('language_manager');
         }
