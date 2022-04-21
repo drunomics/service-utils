8416b88d1a0df6124ec4a108e563db407a5c4faf
tavi toporjinschi <vasike@gmail.com>
2022-04-21 17:04:59 +0300

    LDP-1321: Add DateFormatterTrait.

---
+++ src/Core/Datetime/DateFormatterTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace drunomics\ServiceUtils\Core\File;
+
+use Drupal\Core\File\FileUrlGeneratorInterface;
+
+/**
+ * Allows setter injection and simple usage of the service.
+ */
+trait DateFormatterTrait {
+
+  /**
+   * Date formatter.
+   *
+   * @var \Drupal\Core\Datetime\DateFormatter
+   */
+  protected $dateFormatter;
+
+  /**
+   * Gets the date formatter.
+   *
+   * @todo Add trait for proper dependency injection.
+   *
+   * @return \Drupal\Core\Datetime\DateFormatter
+   *   The date formatter.
+   */
+  public function getDateFormatter() {
+    if (!isset($this->dateFormatter)) {
+      $this->dateFormatter = \Drupal::service('date.formatter');
+    }
+    return $this->dateFormatter;
+  }
+
+}
